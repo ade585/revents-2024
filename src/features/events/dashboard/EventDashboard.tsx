9e55851097e7ae5527fa3e08d56e486304208bad
ade585 <alain.delevingne@dxc.com>
2024-07-02 19:07:47 +0200

    end of section 3

---
+++ src/features/events/dashboard/EventDashboard.tsx
@@ -0,0 +1,34 @@
+import { Grid } from "semantic-ui-react";
+import EventList from "./EventList";
+import EventForm from "../form/EventForm";
+
+import { AppEvent } from "../../../app/types/event";
+import { useEffect, useState } from "react";
+import { sampleData } from "../../../app/layout/api/sampleData";
+
+type Props = {
+  formOpen: boolean
+  setFormOpen: (value: boolean) => void
+}
+
+export default function EventDashboard({ formOpen, setFormOpen }: Props) {
+  const [events, setEvents] = useState<AppEvent[]>([])
+
+  useEffect(() => {
+    setEvents(sampleData);
+  }, [])
+
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <EventList events={events} />
+      </Grid.Column>
+
+      <Grid.Column width={6}>
+        {formOpen &&
+          <EventForm setFormOpen={setFormOpen} />}
+      </Grid.Column>
+    </Grid>
+
+  )
+}
---
+++ src/features/events/form/EventForm.tsx
@@ -0,0 +1,35 @@
+import { Button, Form, Header, Segment } from "semantic-ui-react";
+
+type Props = {
+    setFormOpen: (value : boolean) => void;
+}
+
+export default function EventForm({setFormOpen} : Props ) {
+    return (
+        <Segment>
+            <Header content='Create Events' />
+            <Form>
+                <Form.Field>
+                    <input type="text" placeholder='Event title' />
+                </Form.Field>
+                <Form.Field>
+                    <input type="text" placeholder='Category' />
+                </Form.Field>
+                <Form.Field>
+                    <input type="text" placeholder='Description' />
+                </Form.Field>
+                <Form.Field>
+                    <input type="text" placeholder='City' />
+                </Form.Field>
+                <Form.Field>
+                    <input type="text" placeholder='Venue' />
+                </Form.Field>
+                <Form.Field>
+                    <input type="text" placeholder='Date' />
+                </Form.Field>
+                <Button type="submit" floated='right' positive content="Submit" />
+                <Button onClick= {() => setFormOpen(false)} type='button' floated='right' content='Cancel' />
+            </Form>
+        </Segment>
+    ) 
+}
