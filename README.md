00588418473fcf0c2e78c989b1f5dd6d9b9c4e4f
ade585 <alain.delevingne@dxc.com>
2024-07-03 14:10:15 +0200

    end of section 4

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # revents-2024
 
+mkdir node
+cd node 
+npm create vite@latest
+cd events
+code .
+git config --global user.email "alain.delevingne@dxc.com"
+git config --global user.name "ade585"
+git remote -v
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ade585/revents-2024.git
+git push -u origin main
+npm install semantic-ui-react@3.0.0-beta.2 semantic-ui-css
+npm install --save @paralleldrive/cuid2
+npm run dev
--- src/features/events/dashboard/EventListItem.tsx
+++ src/features/events/dashboard/EventListItem.tsx
@@ -1,37 +1,55 @@
 import { Button, Icon, Item, ItemGroup, List, Segment, SegmentGroup } from "semantic-ui-react";
 import EventListAttendee from "./EventListAttendee";
+import { AppEvent } from "../../../app/types/event";
 
-export default function EventListItem({event}: any) {
-  return (
-    <SegmentGroup>
-        <Segment>
-            <ItemGroup>
-                <Item>
-                    <Item.Image size='tiny' circular src={event.hostPhotoURL} />
-                    <Item.Content>
-                        <Item.Header >{event.title}</Item.Header>
-                        <Item.Description> {event.description}</Item.Description>
-                    </Item.Content>
-                </Item>
+type Props = {
+    event: AppEvent
+    selectEvent: (event: AppEvent) => void;
+    deleteEvent: (eventId: string) => void;
+}
+
+export default function EventListItem({ event, selectEvent, deleteEvent }: any) {
+    return (
+        <SegmentGroup>
+            <Segment>
+                <ItemGroup>
+                    <Item>
+                        <Item.Image size='tiny' circular src={event.hostPhotoURL || './user.png'} />
+                        <Item.Content>
+                            <Item.Header >{event.title}</Item.Header>
+                            <Item.Description> {event.description}</Item.Description>
+                        </Item.Content>
+                    </Item>
                 </ItemGroup>
-        </Segment>
-        <Segment>
-            <span>
-                <Icon name='clock' /> {event.date}
-                <Icon name='marker' /> {event.venue}
-            </span>
-        </Segment>
-        <Segment secondary>
-            <List horizontal>
-                {event.attendees.map((attendee: any) => (
-                    <EventListAttendee key={attendee.id} attendee={attendee}/>
-                ))}
-            </List>
-        </Segment>
-        <Segment clearing>
-            <span> {event.description}</span>
-            <Button color="teal" floated="right" content="View" />
-        </Segment>
+            </Segment>
+            <Segment>
+                <span>
+                    <Icon name='clock' /> {event.date}
+                    <Icon name='marker' /> {event.venue}
+                </span>
+            </Segment>
+            <Segment secondary>
+                <List horizontal>
+                    {event.attendees.map((attendee: any) => (
+                        <EventListAttendee key={attendee.id} attendee={attendee} />
+                    ))}
+                </List>
+            </Segment>
+            <Segment clearing>
+                <span> {event.description}</span>
+                <Button
+                    color="red"
+                    floated="right"
+                    content="Delete"
+                    onClick={() => deleteEvent(event.id)}
+                />
+                <Button
+                    color="teal"
+                    floated="right"
+                    content="View"
+                    onClick={() => selectEvent(event)}
+                />
+            </Segment>
         </SegmentGroup>
-  )
+    )
 }
